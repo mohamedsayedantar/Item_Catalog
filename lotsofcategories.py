b26941d7465e28a32acbf43fd9389b1cb36572b4
mohamedsayedantar <35682211+mohamedsayedantar@users.noreply.github.com>
2018-09-21 14:31:53 +0200

    Update lotsofcategories.py

--- lotsofcategories.py
+++ lotsofcategories.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_create import Categories, Base, Items, User
 
-engine = create_engine('sqlite:///categories.db')
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
